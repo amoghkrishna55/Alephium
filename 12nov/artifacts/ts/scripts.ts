b89060dfda98d02d23343451bcb6808e1a7cc2ab
Amogh Krishna <amoghkrishna55@gmail.com>
2024-11-13 18:52:06 +0530

    Add Withdraw functionality: implement withdraw contract and update Token contract

--- 12nov/artifacts/ts/scripts.ts
+++ 12nov/artifacts/ts/scripts.ts
@@ -12,3 +12,9 @@ import {
   HexString,
 } from "@alephium/web3";
 import { getContractByCodeHash } from "./contracts";
+import { default as WithdrawScriptJson } from "../Withdraw.ral.json";
+
+export const Withdraw = new ExecutableScript<{
+  token: HexString;
+  amount: bigint;
+}>(Script.fromJson(WithdrawScriptJson, "", []), getContractByCodeHash);
